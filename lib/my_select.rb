2362640424f5c66a8949fd95a85a85f16f7614b2
Scott M <gmat191@gmail.com>
2018-10-01 01:06:24 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  i = 0 
  answer = [] 
  
- while count < collection.size 
+ while i < collection.size 
  if yield(collection[i]) == true 
    answer.push(collection[i])
  end 
